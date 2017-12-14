9a9dcb2e052d401ff872b1de7a7356818f1f053a
pkasting <pkasting@google.com>
2017-12-14 01:21:41 -0800

    Finish incomplete cleanup of day 9, add day 13

---
+++ day13.cc
@@ -0,0 +1,80 @@
+// Advent of Code 2017 day 13 solution
+// Peter Kasting, Dec. 13, 2017
+
+#include <algorithm>
+#include <functional>
+#include <iostream>
+#include <regex>
+#include <string>
+#include <utility>
+#include <vector>
+
+namespace {
+
+constexpr bool kPart1 = false;  // Use true for part 1, false for part 2.
+
+// Tokenizes |input|, a string representing a single scanner's depth and range,
+// into a pair of (depth, range).
+std::pair<int, int> ParseScanner(const std::string& input) {
+  const std::regex regex("\\d+");
+  std::sregex_iterator i(input.begin(), input.end(), regex);
+  const int depth = std::stoi(i->str());
+  ++i;
+  return {depth, std::stoi(i->str())};
+}
+
+// Returns whether a packet beginning at |delay| would be detected by |scanner|.
+bool DetectedByScanner(int delay, const std::pair<int, int>& scanner) {
+  // The period of a scanner is 2 * (range - 1).
+  return ((scanner.first + delay) % (2 * (scanner.second - 1))) == 0;
+}
+
+// Returns the severity of a trip beginning with delay |delay|.
+int GetSeverity(const std::vector<std::pair<int, int>>& scanners, int delay) {
+  int severity = 0;
+  for (const auto& i : scanners) {
+    // The severity of getting detected by any single scanner is its depth times
+    // its range.
+    if (DetectedByScanner(delay, i))
+      severity += i.first * i.second;
+  }
+  return severity;
+}
+
+// Returns the minimum necessary delay for a packet to avoid detection by
+// |scanners|.
+int GetDelay(const std::vector<std::pair<int, int>>& scanners) {
+  // This is slow, because it is O(scanners.size()) for each delay value.  It
+  // seems like we ought to be able to do better -- ideally to have the whole
+  // function run in O(scanners.size()) -- but I haven't figured out how.
+  // It seemed promising to incrementing the delay by the least common multiple
+  // of the periods of all successfully-bypassed scanners, instead of by 1, but
+  // this skips some possible solutions.  Maybe something involving Euler's
+  // Theorem or Chinese Remainder Theorem would be useful, but the fact that
+  // each scanner's "window" is more than one time unit wide is making it hard
+  // for me to see how to apply these.
+  int delay = 0;
+  while (std::any_of(scanners.begin(), scanners.end(),
+                     std::bind(DetectedByScanner, delay)))
+    ++delay;
+  return delay;
+}
+
+}  // namespace
+
+int main(int argc, char* argv[]) {
+  std::cout << "Enter program data; terminate with ctrl-z alone on a line."
+            << std::endl;
+
+  std::string input;
+  std::vector<std::pair<int, int>> scanners;
+  while (std::getline(std::cin, input))
+    scanners.push_back(ParseScanner(input));
+
+  if (kPart1)
+    std::cout << "Severity: " << GetSeverity(scanners, 0) << std::endl;
+  else
+    std::cout << "Starting delay: " << GetDelay(scanners) << std::endl;
+
+  return 0;
+}
--- day9.cc
+++ day9.cc
@@ -9,9 +9,9 @@ namespace {
 
 constexpr bool kPart1 = false;  // Use true for part 1, false for part 2.
 
+// Counts the score of the groups in the stream and the number of total garbage
+// characters.  We need the former for part 1 and the latter for part 2.
 std::tuple<int, int> ComputeScores(const std::string& stream) {
-  // Count the score of the groups in the stream and the number of total garbage
-  // characters.  We need the former for part 1 and the latter for part 2.
   int total_score = 0;
   int group_score = 0;
   int garbage_chars = 0;
@@ -44,34 +44,12 @@ int main(int argc, char* argv[]) {
   std::string stream;
   std::cin >> stream;
 
-  // Count the score of the groups in the stream and the number of total garbage
-  // characters.  We need the former for part 1 and the latter for part 2.
-  int total_score = 0;
-  int group_score = 0;
-  int garbage_chars = 0;
-  bool in_garbage = false;
-  for (auto i = stream.begin(); i != stream.end(); ++i) {
-    const char c = *i;
-    // Which checks come before/after the |in_garbage| check is important for
-    // precedence reasons.
-    if (c == '!')
-      ++i;
-    else if (c == '>')
-      in_garbage = false;
-    else if (in_garbage)
-      ++garbage_chars;
-    else if (c == '{')
-      ++group_score;
-    else if (c == '}')
-      total_score += group_score--;
-    else if (c == '<')
-      in_garbage = true;
-  }
+  const auto scores = ComputeScores(stream);
 
   if (kPart1)
-    std::cout << "Score: " << total_score << std::endl;
+    std::cout << "Score: " << std::get<0>(scores) << std::endl;
   else
-    std::cout << "Garbage characters: " << garbage_chars << std::endl;
+    std::cout << "Garbage characters: " << std::get<1>(scores) << std::endl;
 
   return 0;
 }
